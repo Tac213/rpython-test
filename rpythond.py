fb13ba21895f8cdb38a4c188a258ca7daf305dd3
Tac Uchiha <cookiezhx@163.com>
2025-01-29 10:51:49 +0800

    Restore rpythond.py

--- rpythond.py
+++ rpythond.py
@@ -10,7 +10,7 @@ run with --help for more information
 from __future__ import print_function, absolute_import, division
 import sys, os
 sys.path.insert(0, os.path.normpath(os.path.join(__file__, "..", "pypy")))
-# from rpython.translator.goal.translate import main
+from rpython.translator.goal.translate import main
 
 # no implicit targets
 if len(sys.argv) == 1:
@@ -30,5 +30,4 @@ else:
     print("Waiting for a debugging client to attach on the process, localhost:8142")
     debugpy.wait_for_client()
 
-from rpython_ext.rlib import cpython
-# main()
+main()
